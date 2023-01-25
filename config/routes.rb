09684c6ff2b605fb18f9f52c190523835fd5a16a
Natalia Eiflander Braganca <nataliabragancan@gmail.com>
2023-01-25 15:35:38 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  #resources :home do
+   # collection do
+    #  get :about
+    #end
+  #end
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
