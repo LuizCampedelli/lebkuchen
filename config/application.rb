fcee7ccbdaec9c47a4332db6fe490d7f57c7ea01
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2023-03-10 16:37:15 +0100

    gem 18n install

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Lebkuchen
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :de
+    config.i18n.available_locales = [:de, :en]
+    
+
   end
 end
