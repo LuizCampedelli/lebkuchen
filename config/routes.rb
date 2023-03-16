6d01f8c2b469fa1c07f4a60c59dacdea7b08fb28
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2023-03-16 18:35:14 +0100

    fix nav bar w translator

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|de/ do
-    get "event", to: "pages#event"
-    get "findus", to: "pages#findus"
-    get "contact", to: "contacts#contact"
+    get "event", to: "pages#event", as: :event
+    get "findus", to: "pages#findus", as: :findus
+    get "contact", to: "contacts#contact", as: :contact
     post "contact", to: "contacts#form"
     root to: "pages#home"
   end
