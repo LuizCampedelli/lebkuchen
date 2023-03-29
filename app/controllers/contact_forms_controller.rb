60ade8828a6a0fae952f2c4b0d9323b2e19f08ec
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2023-03-29 21:33:05 +0200

    Contact_form controller

---
+++ app/controllers/contact_forms_controller.rb
@@ -0,0 +1,17 @@
+
+class ContactFormsController < ApplicationController
+  def new
+    @contact_form = ContactForm.new
+  end
+
+  def create
+    @contact_form = ContactForm.new(params[:contact_form])
+    if @contact_form.deliver
+      flash[:success] = "Obrigado por entrar em contato conosco! Responderemos em breve."
+      redirect_to root_path
+    else
+      flash[:error] = "Houve um erro ao enviar a mensagem. Por favor, tente novamente."
+      render :new
+    end
+  end
+end
