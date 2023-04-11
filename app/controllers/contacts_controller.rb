309bb52b2e13782c78cc6c16a6aa43955e0fcc57
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2023-04-11 17:26:14 +0200

    form fixed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,19 +4,15 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_form_params)
+    @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = "Message sent"
+      flash.now[:sucess] = "Message sent"
+      render :create
     else
       flash.now[:error] = "Houve um erro ao enviar a mensagem. Por favor, tente novamente."
-      render :new
-    end
-  end
-  
 
-  def contact_form_params
-    params.require(:contact).permit(:name, :email, :message)
+    end
   end
 
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,6 @@
 class Contact < MailForm::Base
   attribute :name, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-
-
   attribute :message, validade: true
   attribute :nickname, captcha: true
 
