9ce823adb91e0276719926717547ecb8afc1e8b0
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2023-04-13 15:29:14 +0200

    scroll reveal

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,8 +13,10 @@ application.register("map", MapController)
 import ModalController from "./modal_controller"
 application.register("modal", ModalController)
 
-
-import ScrollReveal from 'stimulus-scroll-reveal'
-application.register('scroll-reveal', ScrollReveal)
-
-
+import { Application } from 'stimulus';
+import { definitionsFromContext } from 'stimulus/webpack-helpers';
+import ScrollRevealController from './scroll_reveal_controller';
+const application = Application.start();
+const context = require.context('./', true, /_controller\.js$/);
+application.load(definitionsFromContext(context));
+application.register('scroll-reveal', ScrollRevealController);
