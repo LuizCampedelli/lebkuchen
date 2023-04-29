6728ade9c8cc25d56aae458b906d25fe3c0c3005
Luiz Campedelli <vapeprosper@gmail.com>
2023-04-29 11:58:05 -0300

    added Hamburger to X, menu shrink action with stimulus js

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,32 +1,11 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["toggle", "menu", "hamburgerIcon", "navItem"]
-
-  connect() {
-    this.toggleMenu = this.toggleMenu.bind(this)
-    this.hideMenu = this.hideMenu.bind(this)
-
-    this.toggleTarget.addEventListener("click", this.toggleMenu)
-    this.navItemTargets.forEach((navItem) => {
-      navItem.addEventListener("click", this.hideMenu)
-    })
-  }
-
-  disconnect() {
-    this.toggleTarget.removeEventListener("click", this.toggleMenu)
-    this.navItemTargets.forEach((navItem) => {
-      navItem.removeEventListener("click", this.hideMenu)
-    })
-  }
+  static targets = ["toggle", "menu", "hamburgerIcon"];
 
   toggleMenu() {
-    this.menuTarget.classList.toggle("show-menu")
-    this.hamburgerIconTarget.classList.toggle("open")
-  }
-
-  hideMenu() {
-    this.menuTarget.classList.remove("show-menu")
-    this.hamburgerIconTarget.classList.remove("open")
+    this.menuTarget.classList.toggle("show");
+    const isExpanded = this.toggleTarget.getAttribute("aria-expanded") === "true";
+    this.toggleTarget.setAttribute("aria-expanded", !isExpanded);
   }
 }
