297944fb40738054720b6f1f1a18c280126831a6
Luiz Campedelli <vapeprosper@gmail.com>
2023-05-03 19:26:20 -0300

    added errors pages, controller, routes

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Lebkuchen
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.exceptions_app = self.routes # Add this line to configure errors pages
 
     # Configuration for the application, engines, and railties goes here.
     #
